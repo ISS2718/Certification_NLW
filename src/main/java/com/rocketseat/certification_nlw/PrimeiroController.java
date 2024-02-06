ee5c98c35056f8817e2ee3170d338fd78c518c90
Isaac Soares <isaacsantos.soares@hotmail.com>
2024-02-06 01:09:40 -0300

    Primeiro Controller

--- src/main/java/com/rocketseat/certification_nlw/PrimeiroController.java
+++ src/main/java/com/rocketseat/certification_nlw/PrimeiroController.java
@@ -1,5 +1,15 @@
 package com.rocketseat.certification_nlw;
 
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@RestController
+@RequestMapping("/primeiroController")
 public class PrimeiroController {
-    
+    public String retornoPrimeiroController() {
+        return "Criando o Primeiro Controller";
+    }
 }
