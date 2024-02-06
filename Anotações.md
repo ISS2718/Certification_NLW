76a911d098bb2c84d4ae631ebe9abe180111df06
Isaac Soares <isaacsantos.soares@hotmail.com>
2024-02-06 01:41:37 -0300

    Primeiro POST

---
+++ Anotações.md
@@ -0,0 +1,15 @@
+# Minhas Anotações
+
+## API REST
+
+ ### Métodos Comuns
+  - GET: Buscar Informação
+  - PUT: Alterar Informação
+  - POST: Inserir Informação
+  - DELETE: Excluir Informação
+  - PATCH: Aleterar PONTUALMENTE uma Informação
+
+ ### Tipos de Parâmetros
+  - Body: { }
+  - Query Params: http://localhost:8085/users?nome=Isaac&idade=22 (Parâmetros Opcionais)
+  - Route Params: http://localhost:8085/users/{nome}/{idade} (Parâmetros **Obrigatórios**)
--- src/main/java/com/rocketseat/certification_nlw/PrimeiroController.java
+++ src/main/java/com/rocketseat/certification_nlw/PrimeiroController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 
@@ -16,6 +19,11 @@ public class PrimeiroController {
     public Usuario retornoPrimeiroController() {
         var user = new Usuario("Isaac", 22);
         return user;
+    }
+
+    @PostMapping("/meuPrimeiroPost")
+    public String primeiroPost() {
+        return "Meu Primeiro Post";
     }
 
     /**
