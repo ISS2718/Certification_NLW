54e80003e9634ddfb40193440e12346e3e271f57
Isaac Soares <isaacsantos.soares@hotmail.com>
2024-02-06 01:19:41 -0300

    Primeiro Controller

--- src/main/java/com/rocketseat/certification_nlw/PrimeiroController.java
+++ src/main/java/com/rocketseat/certification_nlw/PrimeiroController.java
@@ -4,12 +4,22 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 
 @RestController
 @RequestMapping("/primeiroController")
 public class PrimeiroController {
-    public String retornoPrimeiroController() {
-        return "Criando o Primeiro Controller";
+
+    @GetMapping("/retornarPrimeiroController")
+    public Usuario retornoPrimeiroController() {
+        var user = new Usuario("Isaac", 22);
+        return user;
     }
+
+    /**
+     * Usuario
+     */
+    public record Usuario(String nome, int idade) {}
 }
